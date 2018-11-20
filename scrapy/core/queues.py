4968f966adc39ca141a6016c0ed0d07501e8c273
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2018-11-20 16:13:24 +0000

    wrappers for every disk queue class

--- scrapy/core/queues.py
+++ scrapy/core/queues.py
@@ -2,6 +2,14 @@ import uuid
 import os.path
 
 
+from queuelib.queue import (
+        FifoDiskQueue,
+        LifoDiskQueue,
+        FifoSQLiteQueue,
+        LifoSQLiteQueue,
+        )
+
+
 def unique_files_queue(queue_class):
 
     class UniqueFilesQueue(queue_class):
@@ -13,3 +21,9 @@ def unique_files_queue(queue_class):
             super().__init__(path)
 
     return UniqueFilesQueue
+
+
+UniqueFileFifoDiskQueue = unique_files_queue(FifoDiskQueue)
+UniqueFileLifoDiskQueue = unique_files_queue(LifoDiskQueue)
+UniqueFileFifoSQLiteQueue = unique_files_queue(FifoSQLiteQueue)
+UniqueFileLifoSQLiteQueue = unique_files_queue(LifoSQLiteQueue)
