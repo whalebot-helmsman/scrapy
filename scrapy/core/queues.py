e1877c0ccf0efe67579cf4d1835a9f6af4a74e2d
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2018-11-20 16:21:18 +0000

    round-robin implementation without I/O and slot detection

--- scrapy/core/queues.py
+++ scrapy/core/queues.py
@@ -1,13 +1,19 @@
+from collections import deque
+import logging
 import uuid
 import os.path
 
 
+from queuelib import PriorityQueue
 from queuelib.queue import (
         FifoDiskQueue,
         LifoDiskQueue,
         FifoSQLiteQueue,
         LifoSQLiteQueue,
         )
+
+
+logger = logging.getLogger(__name__)
 
 
 def unique_files_queue(queue_class):
@@ -27,3 +33,57 @@ UniqueFileFifoDiskQueue = unique_files_queue(FifoDiskQueue)
 UniqueFileLifoDiskQueue = unique_files_queue(LifoDiskQueue)
 UniqueFileFifoSQLiteQueue = unique_files_queue(FifoSQLiteQueue)
 UniqueFileLifoSQLiteQueue = unique_files_queue(LifoSQLiteQueue)
+
+
+SCHEDULER_SLOT_META_KEY = 'scheduler_slot'
+
+
+def scheduler_slot(request):
+    meta = dict()
+    if isinstance(request, dict):
+        meta = request.get('meta', dict())
+    else:
+        meta = getattr(request, 'meta', dict())
+
+    slot = meta.get(SCHEDULER_SLOT_META_KEY, None)
+    return slot
+
+
+class RoundRobinQueue:
+
+    def __init__(self, qfactory, startprios=()):
+        self._slots = deque()
+        self.pqueues = dict()     # slot -> priority queue
+        self.qfactory = qfactory  # factory for creating new internal queues
+
+    def push(self, request, priority):
+
+        slot = scheduler_slot(request)
+        logger.debug('Push "%s"', slot)
+        if slot not in self.pqueues:
+            self.pqueues[slot] = PriorityQueue(self.qfactory)
+            self._slots.append(slot)
+        self.pqueues[slot].push(request, priority)
+
+    def pop(self):
+        if not self._slots:
+            return
+        slot = self._slots.popleft()
+        logger.debug('Pop "%s"', slot)
+        queue = self.pqueues[slot]
+        request = queue.pop()
+
+        if len(queue):
+            self._slots.append(slot)
+        else:
+            del self.pqueues[slot]
+        return request
+
+    def close(self):
+        for queue in self.pqueues.values():
+            queue.close()
+        self.pqueues.clear()
+        self._slots.clear()
+
+    def __len__(self):
+        return sum(len(x) for x in self.pqueues.values()) if self.pqueues else 0
