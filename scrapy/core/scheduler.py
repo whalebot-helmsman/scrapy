e699daf66d19eb5f400aefd307f7f4c019511c7e
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2018-11-28 10:22:54 +0000

    better way to get name

--- scrapy/core/scheduler.py
+++ scrapy/core/scheduler.py
@@ -111,7 +111,7 @@ class Scheduler(object):
         return self.mqclass()
 
     def _newdq(self, priority):
-        return self.dqclass(join(self.dqdir, 'p%s' % priority))
+        return self.dqclass(join(self.dqdir, 'p%s' % (priority, )))
 
     def _dq(self):
         activef = join(self.dqdir, 'active.json')
